121cdcf735111cf67859a1284eb1f8c7b2c94a5f
Arben <arben.dedaj1@studen.uni-pr.edu>
2021-03-20 23:09:11 +0100

    eeee

--- WebApp/index.php
+++ WebApp/index.php
@@ -2,99 +2,10 @@
 <html lang="en">
 <head>
     <link rel="stylesheet" href="Asets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="index.css">
+
     <meta charset="UTF-8">
     <title>Crime Reporting System</title>
-    <style>
-        body{
-
-            background:#f4f4f4;
-        }
-
-        .navbar{
-            border-radius: 0;
-        }
-
-        .navbar-nav >li {
-            padding-left:20px;
-        }
-
-        .navbar-right >li {
-            padding-left:20px;
-        }
-
-        .navbar-default {
-            background-color: #005b96;
-            border-color: #03396c;
-        }
-        .navbar-default .navbar-brand {
-            color: #ecf0f1;
-        }
-        .navbar-default .navbar-brand:hover,
-        .navbar-default .navbar-brand:focus {
-            color: #b3cde0;
-        }
-        .navbar-default .navbar-text {
-            color: #ecf0f1;
-        }
-        .navbar-default .navbar-nav > li > a {
-            color: #ecf0f1;
-        }
-        .navbar-default .navbar-nav > li > a:hover,
-        .navbar-default .navbar-nav > li > a:focus {
-            color: #b3cde0;
-        }
-        .navbar-default .navbar-nav > .active > a,
-        .navbar-default .navbar-nav > .active > a:hover,
-        .navbar-default .navbar-nav > .active > a:focus {
-            color: #b3cde0;
-            background-color: #03396c;
-        }
-        .navbar-default .navbar-nav > .open > a,
-        .navbar-default .navbar-nav > .open > a:hover,
-        .navbar-default .navbar-nav > .open > a:focus {
-            color: #b3cde0;
-            background-color: #03396c;
-        }
-        .navbar-default .navbar-toggle {
-            border-color: #03396c;
-        }
-        .navbar-default .navbar-toggle:hover,
-        .navbar-default .navbar-toggle:focus {
-            background-color: #03396c;
-        }
-        .navbar-default .navbar-toggle .icon-bar {
-            background-color: #ecf0f1;
-        }
-        .navbar-default .navbar-collapse,
-        .navbar-default .navbar-form {
-            border-color: #ecf0f1;
-        }
-        .navbar-default .navbar-link {
-            color: #ecf0f1;
-        }
-        .navbar-default .navbar-link:hover {
-            color: #b3cde0;
-        }
-
-
-
-        @media (max-width: 767px) {
-            .navbar-default .navbar-nav .open .dropdown-menu > li > a {
-                color: #ecf0f1;
-            }
-            .navbar-default .navbar-nav .open .dropdown-menu > li > a:hover,
-            .navbar-default .navbar-nav .open .dropdown-menu > li > a:focus {
-                color: #b3cde0;
-            }
-            .navbar-default .navbar-nav .open .dropdown-menu > .active > a,
-            .navbar-default .navbar-nav .open .dropdown-menu > .active > a:hover,
-            .navbar-default .navbar-nav .open .dropdown-menu > .active > a:focus {
-                color: #b3cde0;
-                background-color: #03396c;
-            }
-        }
-
-    </style>
 </head>
 <body>
 <header>
@@ -102,7 +13,8 @@
     <nav class="navbar navbar-default">
         <div class="container">
             <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
+                        aria-expanded="false" aria-controls="navbar">
                     <span class="sr-only">Toggle navigation</span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
@@ -111,7 +23,7 @@
             </div>
             <div id="navbar" class="collapse navbar-collapse">
                 <ul class="nav navbar-nav">
-                    <li class="active"><a href="#">Home</a></li>
+                    <li class="active"><a href="#"><span class=" glyphicon glyphicon-eye-open"aria-hidden="true"></span> Home</a></li>
 
                 </ul>
 
@@ -123,8 +35,12 @@
         </div>
     </nav>
 </header>
-<script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
+
+<script src="https://code.jquery.com/jquery-1.12.4.min.js"
+        integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ"
+        crossorigin="anonymous"></script>
 <script src="Asets/js/bootstrap.min.js"></script>
+
 
 <div class="container">
 
@@ -162,7 +78,6 @@
         </a>
     </div>
 </div>
-
 
 
 <div style="height: 400px"></div>
