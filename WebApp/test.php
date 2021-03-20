a85e8e81d921fb6e9900459abfa21f047a2d71a1
Andi <andi.dika@student.uni-pr.edu>
2021-03-20 15:43:09 +0100

    add test.php

---
+++ WebApp/test.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>|Contact Us|</title>
+    <link rel="stylesheet" href="style.css"/>
+
+    <style>
+        h1{
+            color:red;
+        }
+
+        div{
+            border: 1px solid black;
+
+        }
+    </style>
+
+</head>
+<body>
+
+<h1>Andi bukuroshi</h1>
+<div>
+
+<p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum harum ipsum tempora. Asperiores aut eveniet excepturi facilis illo inventore, iure nulla quas sapiente sunt voluptas, voluptatem voluptates. Cupiditate libero, magnam.
+</p>
+</div></dov>
+
+
+</body>
