f7dc95db901320bd22c99913227e43248965ba17
Andi <andi.dika@student.uni-pr.edu>
2021-03-21 02:38:37 +0100

    sidebar

--- WebApp/AdminPanel/adminPanel.php
+++ WebApp/AdminPanel/adminPanel.php
@@ -27,4 +27,37 @@ include "navbar.php";
     </div>
 </header>
 
+<section id="main">
+    <div class ="container">
+        <div class="row">
+            <div class="col-md-3">
+                <div class="list-group">
+                    <a href="adminPanel.php" class="list-group-item active main-color-bg" ><span class ="glyphicon glyphicon-cog" aria-hidden=""true> </span>
+                        Dashboard
+                    </a>
+                    <a href="users.php" class="list-group-item"><span class ="glyphicon glyphicon-user" aria-hidden=""true> </span>Users<span class="badge">12</span></a>
+                    <a href="reports.php" class="list-group-item"><span class ="glyphicon glyphicon-cog" aria-hidden=""true> </span>Reports<span class="badge">12</span></a>
+                    <a href="feedback.php" class="list-group-item"><span class ="glyphicon glyphicon-pencil" aria-hidden=""true> </span>Feedback<span class="badge">12</span></a>
+                </div>
 
+                <div class="well">
+                    <h4>Number of reports resolved</h4>
+                    <div class="progress">
+                        <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
+                            60%
+                        </div>
+                    </div>
+                    <h4>lorem lorem</h4>
+                    <div class="progress">
+                        <div class="progress-bar" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40 %;">
+                            40%
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-9">
+
+            </div>
+        </div>
+    </div>
+</section>
