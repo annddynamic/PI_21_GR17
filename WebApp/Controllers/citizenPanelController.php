ef93dd236ca6a4b754eb216ed898863b5e91043d
Arben <arben.dedaj1@studen.uni-pr.edu>
2021-05-15 22:59:18 +0200

    Revert "SDD"
    This reverts commit e7f0bb3d

--- WebApp/Controllers/citizenPanelController.php
+++ WebApp/Controllers/citizenPanelController.php
@@ -69,39 +69,49 @@ class citizenPanelController extends Controller
 
                 // require_once('PHPMailerAutoload.php');
                 
-                $mail = new PHPMailer;
+                 $mail = new PHPMailer();
  
-                $mail->SMTPDebug = 4;                               // Enable verbose debug output
-                
-                $mail->isSMTP();                                      // Set mailer to use SMTP
-                $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+//                $mail->SMTPDebug = 4;                               // Enable verbose debug output
+//
+//                $mail->isSMTP();                                      // Set mailer to use SMTP
+//                $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+//                $mail->SMTPAuth = true;                               // Enable SMTP authentication
+//                $mail->Username = 'anndidika2001@gmail.com';                 // SMTP username
+//                $mail->Password = 'say my name';                           // SMTP password
+//                $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+//                $mail->Port = 587;                                    // TCP port to connect to
+//
+//                $mail->setFrom('anndidika2001@gmail.com', 'CrimeReportSystem');
+//                $mail->addAddress($data['to'] );     // Add a recipient
+////                $mail->addAddress('ellen@example.com');               // Name is optional
+//                // $mail->addCC('cc@example.com');
+//                // $mail->addBCC('bcc@example.com');
+//
+//                // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//                // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+//                $mail->isHTML(true);                                  // Set email format to HTML
+//
+//                $mail->Subject = $data['subject'];
+//                $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+//                $mail->AltBody = $data['message'];
+
+
+                $mail->isSMTP();
                 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-                $mail->Username = 'adnit.gashi@student.uni-pr.edu';                 // SMTP username
-                $mail->Password = 'adnitgashigoogle12';                           // SMTP password
-                $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-                $mail->Port = 587;                                    // TCP port to connect to
-                
-                $mail->setFrom('adnit.gashi@student.uni-pr.edu', 'CrimeReportSystem');
-                $mail->addAddress($data['to'] );     // Add a recipient
-                $mail->addAddress('ellen@example.com');               // Name is optional
-                $mail->addReplyTo('adnit.gashi@student.uni-pr.edu');
-                // $mail->addCC('cc@example.com');
-                // $mail->addBCC('bcc@example.com');
-                
-                // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-                // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-                $mail->isHTML(true);                                  // Set email format to HTML
-                
-                $mail->Subject = $data['subject'];
-                $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-                $mail->AltBody = $data['message'];
-                
-                if(!$mail->send()) {
-                    echo 'Message could not be sent.';
-                    echo 'Mailer Error: ' . $mail->ErrorInfo;
-                } else {
-                    echo 'Message has been sent';
-                }
+                $mail->SMTPSecure='ssl';
+                $mail->Host='smtp.gmail.com';
+                $mail->Port='465';
+                $mail->isHTML();
+                $mail->Username='crimereportsystem69@gmail.com';
+                $mail->Password='alfa alfa123';
+                $mail->SetFrom('crimereportsystem69@gmail.com','Crime-Report-System');
+                $mail->addReplyTo('crimereportsystem69@gmail.com');
+                $mail->Subject=$data['subject'];
+                $mail->Body=$data['message'];
+                $mail->AddAddress($data['to']);
+                $mail->Send();
+
+
 
             }
             
